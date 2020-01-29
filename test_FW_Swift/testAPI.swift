090fc8cf930836a7ed96ac7b870d668f789df19d
abhin <abhinandan.singh@UE-B-NOTE-0040.local>
2020-01-29 16:58:16 +0530

    first commit '

---
+++ test_FW_Swift/testAPI.swift
@@ -0,0 +1,15 @@
+//
+//  testAPI.swift
+//  test_FW_Swift
+//
+//  Created by Abhinandan Singh on 29/01/20.
+//  Copyright © 2020 Abhinandan Singh. All rights reserved.
+//
+
+import Foundation
+
+public class api {
+    public class func logToConsole(msg: String) {
+        print(msg)
+    }
+}
---
+++ test_FW_Swift/test_FW_Swift.h
@@ -0,0 +1,19 @@
+//
+//  test_FW_Swift.h
+//  test_FW_Swift
+//
+//  Created by Abhinandan Singh on 29/01/20.
+//  Copyright © 2020 Abhinandan Singh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for test_FW_Swift.
+FOUNDATION_EXPORT double test_FW_SwiftVersionNumber;
+
+//! Project version string for test_FW_Swift.
+FOUNDATION_EXPORT const unsigned char test_FW_SwiftVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <test_FW_Swift/PublicHeader.h>
+
+
